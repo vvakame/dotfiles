48ee708a81cdcdb68eb32b4b9e265ff7297f09db
vvakame <vvakame+dev@gmail.com>
2010-11-14 19:02:21 +0900

    dropbox.shの修正

--- dropbox.sh
+++ dropbox.sh
@@ -2,6 +2,8 @@
 
 mkdir ~/work
 ln -s ~/Dropbox/work/DroppShare ~/work
+ln -s ~/Dropbox/work/AndroidUtil ~/work
+ln -s ~/Dropbox/work/AndroidUtilTest ~/work
 ln -s ~/Dropbox/work/PolkoDotter ~/work
 ln -s ~/Dropbox/work/BillingTest ~/work
 ln -s ~/Dropbox/work/JsonIntent ~/work
