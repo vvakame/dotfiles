c36d7dfae0a6f1fe06bceda2e4e60439856d9d44
vvakame <vvakame+dev@gmail.com>
2014-08-04 12:29:24 +0900

    brew.sh を追加

---
+++ brew.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -eu
+
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
+brew install the_silver_searcher
+brew install tree
+brew install curl
+brew install wget
+brew install zsh
+brew install zsh-completions
+
+brew install git
+brew install hub
+brew install ant
+brew install maven
+brew install jq
+
+brew install emacs
+brew install go
+brew install phantomjs
+brew install sqlite
+
+brew install caskroom/cask/brew-cask
+
+brew cask install android-studio
+brew cask install eclipse-ide
+brew cask install webstorm
+
+brew cask install iterm2
+brew cask install google-japanese-ime
+brew cask install sublime-text
+brew cask install coteditor
+brew cask install atom
+brew cask install sublime-text
+brew cask install google-chrome
+brew cask install firefox
+brew cask install sourcetree
+brew cask install trailer
+brew cask install virtualbox
+
+brew cask install dropbox
+brew cask install evernote
+brew cask install skitch
+brew cask install yorufukurou
+brew cask install kobito
+brew cask install onepassword
+brew cask install alfred
