40d3b37886ffc291adf6d7314fefa8f60d9b80a0
vvakame <vvakame+dev@gmail.com>
2014-08-04 23:13:23 +0900

    setup.sh 追記

--- setup.sh
+++ setup.sh
@@ -3,9 +3,16 @@ git submodule update
 ./ln.sh
 ./brew.sh
 
+# Macの設定
+defaults write com.apple.dock autohide -bool Yes; killall Dock
+
 # ssh公開鍵をいろいろなところに配る
 # JDK 入れる
 # brew cask alfred link
+
+# iterm2でGeneral -> Preferences で Dropboxのsettingsフォルダのあそこ指定する
+
+# sudo vi /etc/shells に /usr/local/bin/zsh 追加して chsh -s /usr/local/bin/zsh
 
 # nodebrew と rbenv 設定
 # rbenv install 1.9.3-p547
