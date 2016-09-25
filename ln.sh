8b56391eb09d0f782371de6ff37e52da90bd601f
vvakame <vvakame+dev@gmail.com>
2016-09-25 13:20:35 +0000

    split .gitconfig per env

--- ln.sh
+++ ln.sh
@@ -2,5 +2,10 @@
 ln -s $HOME/dotfiles/.zshrc $HOME/
 ln -s $HOME/dotfiles/.screenrc $HOME/
 ln -s $HOME/dotfiles/.gitconfig $HOME/
+if [ `uname` = "Darwin" ]; then
+  ln -s $HOME/dotfiles/.gitconfig.env.mac $HOME/.gitconfig.env
+elif [ `uname` = "Linux" ]; then
+  ln -s $HOME/dotfiles/.gitconfig.env.linux $HOME/.gitconfig.env
+fi
 ln -s $HOME/dotfiles/.emacs $HOME/
 # ln -s ~/src/dotfiles/.vimrc ~/
