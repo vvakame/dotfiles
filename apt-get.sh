dbaa0b418230fc3aa840ff1f4a77f6d1ca869011
vvakame <vvakame+dev@gmail.com>
2016-09-28 19:21:30 +0900

    apt-get.sh 更新

--- apt-get.sh
+++ apt-get.sh
@@ -5,8 +5,9 @@ sudo apt-get install -y git zsh
 
 # for h2o
 sudo apt-get install -y g++ cmake openssl pkg-config zlib1g-dev libuv-dev
+# for rotatelogs
+sudo apt-get install -y apache2-utils
 
 sudo chmod +x /usr/share/doc/git/contrib/diff-highlight/diff-highlight
 
 sudo apt-get install -y letsencrypt
-
