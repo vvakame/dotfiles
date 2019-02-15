bf217e61a9386267c4d25679c46b6a056c927274
vvakame <vvakame+dev@gmail.com>
2019-02-16 00:30:51 +0900

    使ってるアプリ類の更新

--- others.sh
+++ others.sh
@@ -3,22 +3,28 @@
 open https://www.iterm2.com/downloads.html
 open https://www.alfredapp.com/
 open http://www.oracle.com/technetwork/java/javase/downloads/index.html
-open https://docs.docker.com/docker-for-mac/
+open https://hub.docker.com/editions/community/docker-ce-desktop-mac
 open https://www.tug.org/mactex/
 open https://atom.io/
 open https://code.visualstudio.com/
+open https://www.sketchapp.com/
 open https://1password.com/
+open https://discordapp.com/
+open https://store.steampowered.com/about/
 
-# Evernote
-open https://itunes.apple.com/jp/app/evernote-stay-organized/id406056744?l=en&mt=12
+# Xcode
+open https://itunes.apple.com/jp/app/xcode/id497799835?l=en&mt=12
 # Slack
 open https://itunes.apple.com/jp/app/slack/id803453959?l=en&mt=12
 # NightOwl
 open https://itunes.apple.com/jp/app/night-owl/id428834068?l=en&mt=12
-
+# Affinity Designer
+open https://itunes.apple.com/jp/app/affinity-designer/id824171161?l=en&mt=12
+# Skitch
+open https://itunes.apple.com/jp/app/skitch-snap-mark-up-share/id425955336?l=en&mt=12
 
 # Google Cloud SDK
-wget -O $HOME/google-cloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-159.0.0-darwin-x86_64.tar.gz
+wget -O $HOME/google-cloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-234.0.0-darwin-x86_64.tar.gz
 tar xvzf $HOME/google-cloud-sdk.tar.gz -C $HOME
 $HOME/google-cloud-sdk/install.sh --quiet
 gcloud components update
@@ -26,9 +32,10 @@ gcloud components install beta -q
 
 # node.js
 curl -L git.io/nodebrew | perl - setup
-nodebrew install-binary v8
-nodebrew use v8
+nodebrew install-binary v11
+nodebrew use v11
 npm install -g typescript typescript-formatter yarn @angular/cli
+npm install -g typescript typescript-formatter yarn firebase-tools live-server @angular/cli
 
 # rustup
 curl https://sh.rustup.rs -sSf | sh -s -- -y
